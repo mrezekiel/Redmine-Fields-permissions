82df4d3dba2b83ab9baa6b04d4993c4338e0707c
Joel Thompson <joel.thompson@macquarietextiles.com.au>
2012-08-21 15:59:35 +1000

    Start date is now also hidden

--- init.rb
+++ init.rb
@@ -2,11 +2,11 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_fields_permissions do
   name 'Redmine Fields Permissions Plugin'
-  author 'Romain E SILVA (Sysdream)'
-  description 'This Redmine plugin add additional permissions for fields in workflow. This plugin is based on http://9thport.net/2011/03/20/redmine-hide-assigned-to-field-with-role-permissions-plugin/ by Aaron Addleman'
-  version '1.0.0'
-  url 'http://www.sysdream.com/'
-  author_url 'http://www.sysdream.com/'
+  author 'Joel Thompson (Macquarie Textiles)'
+  description 'This Redmine plugin add additional permissions for fields in workflow. This plugin is based on https://github.com/resilva/Redmine-Fields-permissions'
+  version '1.0.1'
+  url 'https://github.com/mrezekiel/Redmine-Fields-permissions'
+  author_url 'http://www.macquarietextles.com.au'
 
   # Permissions for assigned to field
   permission :edit_assigned_to, :issues => :index
@@ -22,5 +22,8 @@ Redmine::Plugin.register :redmine_fields_permissions do
 
   # Permissions for fixed version field
   permission :edit_fixed_version, :issues => :index
+  
+  #Permissions for start date field
+  permission :edit_start_date, :issues => :index
 end
 
